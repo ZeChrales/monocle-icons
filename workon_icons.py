e9dc2297bc41e7100ab5d1b0bf62745a8bf48d15
wormsky <n.nicolas.clement@gmail.com>
2017-12-24 11:42:21 +0100

    Generating larger icons

--- workon_icons.py
+++ workon_icons.py
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+function outline {
+    convert $1 -channel A -virtual-pixel transparent -morphology EdgeOut Diamond outside.png
+    convert outside.png -threshold 100% outline.png
+    convert $1 outline.png -compose Over -composite $2
+    rm outside.png outline.png
+}
+
 
 for image in original-icons/*.png
 do
     name=${image##*/}
-    convert $image -fuzz 0.5% -trim +repage -resize 30x30\> -sharpen 0x1.4 -background none -gravity center -extent 30x30 resized.png
-    convert resized.png -channel A -virtual-pixel transparent -morphology EdgeOut Diamond outside.png
-    convert outside.png -threshold 100% outline.png
-    convert resized.png outline.png -compose Over -composite icons/$name 
-    echo $name moving to icons/$name
-    rm resized.png outside.png outline.png
+    convert $image -fuzz 0.5% -trim +repage -resize 30x30\> -sharpen 0x1.4 -background none -gravity center -extent 30x30 icon.png
+    convert $image -fuzz 0.5% -trim +repage -resize 94x94\> -sharpen 0x1.4 -background none -gravity center -extent 94x94 larger.png
+    outline icon.png icon-outlined.png 
+    outline larger.png larger-outlined.png
+    rm icon.png larger.png
+    mv icon-outlined.png icons/$name
+    mv larger-outlined.png larger-icons/$name
 done
-
-#convert original-icons/1.png -fuzz 0.5% -trim +repage -resize 30x30\> -sharpen 0x1.4 -background none -gravity center -extent 30x30 1_resize.png
-#convert 1_resize.png -channel A -virtual-pixel transparent -morphology EdgeOut Diamond 1_out.png
-#convert 1_resize.png 1_out_black.png -compose Over -composite 1_resize_outline.png
-
-#eog 1_resize_border.png &
