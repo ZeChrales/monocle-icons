a87e5d117929b29ab91466bc2546da657d8c8e2d
wormsy <wormsy@protonmail.com>
2018-05-24 14:08:09 +0200

    Adding ability to add argument to script

--- workon_icons.sh
+++ workon_icons.sh
@@ -7,15 +7,27 @@ function outline {
     rm outside.png outline.png
 }
 
-
-for image in original-icons/*.png
-do
-    name=${image##*/}
-    convert $image -fuzz 0.5% -trim +repage -resize 30x30\> -sharpen 0x1.4 -background none -gravity center -extent 30x30 icon.png
-    convert $image -fuzz 0.5% -trim +repage -resize 94x94\> -sharpen 0x1.4 -background none -gravity center -extent 94x94 larger.png
+function workon {
+    name=${1##*/}
+    convert $1 -fuzz 0.5% -trim +repage -resize 30x30\> -sharpen 0x1.4 -background none -gravity center -extent 30x30 icon.png
+    convert $1 -fuzz 0.5% -trim +repage -resize 94x94\> -sharpen 0x1.4 -background none -gravity center -extent 94x94 larger.png
     outline icon.png icon-outlined.png 
     outline larger.png larger-outlined.png
     rm icon.png larger.png
     mv icon-outlined.png icons/$name
     mv larger-outlined.png larger-icons/$name
-done
+}
+
+if [ $# -eq 0 ]; then
+    for image in original-icons/*.png
+    do
+        workon $image
+    done
+    exit 0
+elif [ $# -eq 1 ]; then 
+    workon $1
+    exit 0
+fi
+
+exit 0
+
